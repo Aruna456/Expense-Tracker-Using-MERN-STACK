67075af3b4d6517bd150448ec300540c0452fd9f
Aruna <140523109+Aruna456@users.noreply.github.com>
2024-08-06 14:52:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const { get_Labels } = require('./controller/controller');
 const port = process.env.PORT || 5000;
 
 // Use middleware
-app.use(cors());
+app.use(cors(
+    {
+        orgin: [""],
+        methods:["POST","GET"],
+        credentials:true;
+    }
+));
 app.use(express.json());
 
 // MongoDB connection
